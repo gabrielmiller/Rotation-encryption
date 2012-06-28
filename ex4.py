ef1867671ba4d8ffec58508a2b6ee2741e292ec2
L Gabriel Miller <gmiller2007@gmail.com>
2012-06-28 17:59:47 -0400

    Responded to some of my initial criticisms

--- ex4.py
+++ ex4.py
@@ -1,44 +1,58 @@
-TheData=raw_input("Enter your data > ")
-TheData=str(TheData[:])
-lendiff=len(TheData)%8
-if lendiff>0:
-  TheData=TheData+((8-lendiff)*'0')
+import copy #Import copy because deepcopy is used
  
-DataList = list(TheData)
-DataListBinary = []
-atemp=[]
-DataListBinaryOutput = [[],[],[],[],[],[],[],[]]
-indexer = [0,0]
-for sublst in DataListBinaryOutput:
-  sublst=[[],[],[],[],[],[],[],[]]
+class dataChunk():
+    def __init__(self, chunk):
+        self.chunk = chunk
+    def breakToList(self):  #Breaks strings into lists of strings, and lists of strings into lists of lists of strings
+        try:
+            for num in range(len(self.chunk)):
+                self.chunk[num]=list(self.chunk[num])
+        except:
+            self.chunk=list(self.chunk)
+    def stringToBinary(self):   #Converts each character into binary and replaces its "0b" with "00" if applicable
+        for num in range(len(self.chunk)):
+            self.chunk[num]='0'*(8-ord(self.chunk[num]).bit_length())+bin(ord(self.chunk[num])).lstrip('0b')
+    def flipData(self): #Interprets data from a shifted 90 degree perspective
+        self.temp = copy.deepcopy(self.chunk)
+        for num in range(len(self.chunk)):
+            for innum in range(len(self.chunk[num])):
+                self.temp[num][innum]=self.chunk[innum][num]
+        self.chunk=self.temp
+    def unbreakList(self):  #Joins lists of lists of strings into lists of strings, and lists of strings into strings
+        try:
+            self.chunk=''.join(self.chunk)
+        except:
+            for num in range(len(self.chunk)):
+                self.chunk[num]=''.join(self.chunk[num])
+    def binaryToString(self):   #Converts binary strings into single character strings
+        for num in range(len(self.chunk)):
+            self.chunk[num]=chr(int(self.chunk[num],2))
+    def createOutput(self): #Returns output to the user
+        return self.chunk
  
-for a in DataList:
-  for b in bin(ord(a)):
-    atemp.append(b)
-  DataListBinary.append(atemp)
-  atemp=[]
+def main():
+    enteredData = raw_input("Enter your data > ")
+    modDiff = len(enteredData)%8        #Makes sure our chunks are each 8 characters.
+    eData = enteredData[:]              #If they aren't then the difference in 0s is appended to the string.
+    if modDiff>0:
+        eData = enteredData[:]+(8-modDiff)*'0'
+    chunkDict = {}
+    counter = 0
+    while len(eData)>0:                  #Create a dict of 8 character chunks and instance their values.
+        chunkDict[counter]=dataChunk(eData[:8])
+        eData = eData[8:]
+        counter+=1
+    output=''
+    for item in chunkDict:              #For every 8 character chunk, iterate through the methods and concatenate output
+        chunkDict[item].breakToList()
+        chunkDict[item].stringToBinary()
+        chunkDict[item].breakToList()
+        chunkDict[item].flipData()
+        chunkDict[item].unbreakList()
+        chunkDict[item].binaryToString()
+        chunkDict[item].unbreakList()
+        output=output[:]+chunkDict[item].createOutput()
+    print "Your output is \'%s\'" % output
  
-for c in DataListBinary:
-  offset=c.index('b')
-  c[:offset+1]=offset*['0']
-  if len(c)<8:
-    c=((8-len(c))*['0'])+c
-  for d in c:
-    DataListBinaryOutput[indexer[1]].insert(indexer[0],d)
-    indexer[1]=indexer[1]+1
-  indexer[0]=indexer[0]+1
-  indexer[1]=0
- 
-indexer = [0,0]
-joiner=['','','','','','','','']
-for e in DataListBinaryOutput:
-  for f in e:
-    joiner[indexer[0]]=joiner[indexer[0]][:]+f
-    indexer[1]=indexer[1]+1
-  indexer[1]=0
-  indexer[0]=indexer[0]+1
- 
-finalResult=''
-for g in joiner:
-  finalResult=finalResult[:]+chr(int(g, 2))
-print finalResult
+if __name__ == "__main__":
+    main()
